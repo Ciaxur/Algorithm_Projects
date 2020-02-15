bde85dd643c476b4363cb24ef4404e9208fd73b7
Omar Omar <omar41097@gmail.com>
2020-02-14 21:20:54 -0500

    Added Interactive Features

--- 01-A_Star Search (JS)/sketch.js
+++ 01-A_Star Search (JS)/sketch.js
@@ -1,9 +1,10 @@
+let startAlgo = false;
 let grid;
 let A_Star;
 
 let rows = 75;
 let cols = 75;
-const wallRate = 0.5;
+const wallRate = 0.3;
 let neighbors;
 
 function setup() {
@@ -46,8 +47,34 @@ function draw() {
     grid.drawBlocks(A_Star.getBlockArray());
 
     // Run the Algorithm
-    if (!A_Star.isDone)
-        A_Star.run();
+    if (startAlgo) {
+        if (!A_Star.isDone)
+            A_Star.run();
+        else {
+            noLoop();
+        }
+    }
 }
 
 
+// Draw Wall
+function mouseDragged() {
+    // Check Mouse is Within Canvas
+    if (mouseX < width  && mouseX >= 0 &&
+        mouseY < height && mouseY >= 0) {
+        
+        const x = floor(map(mouseX, 0, width, 0, rows));
+        const y = floor(map(mouseY, height, 0, cols, 0));
+        const blockArr = A_Star.getBlockArray();
+        
+        blockArr[x][y].setWall(true);
+    }
+}
+
+
+// Begin Algorithm Button
+window.addEventListener('load',() => {
+    document.getElementById("algoTrigger").addEventListener('click', () => {
+        startAlgo = !startAlgo;
+    })
+});
